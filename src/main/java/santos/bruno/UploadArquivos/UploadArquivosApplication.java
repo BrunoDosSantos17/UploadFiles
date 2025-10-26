7359b1a19d25efd12d5f5be3bb7447da303ed27f
Bruno dos Santos <bruno.coelho.santos17@gmail.com>
2025-10-26 19:04:51 -0300

    firt commit

---
+++ src/main/java/santos/bruno/UploadArquivos/UploadArquivosApplication.java
@@ -0,0 +1,13 @@
+package santos.bruno.UploadArquivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadArquivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadArquivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/santos/bruno/UploadArquivos/UploadArquivosApplicationTests.java
@@ -0,0 +1,13 @@
+package santos.bruno.UploadArquivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadArquivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
